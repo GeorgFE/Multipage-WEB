14ed9ac6c10f452433266b84f57b05c551a7ad2b
GeorgFE <89256345+GeorgFE@users.noreply.github.com>
2022-08-14 21:42:24 +0300

    no menya eto ne ostanovit)

---
+++ js/script1.js
@@ -0,0 +1,8 @@
+console.log('check the problem to connect js file')
+let biliboba='spongebob'
+console.log(biliboba+' and patrick');
+function frog(){
+    console.log('kva-')
+    console.log('kva')
+}
+frog()
